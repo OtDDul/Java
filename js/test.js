ef23f1d16bb89c64ba7a3839258381f2efde8445
Cha <wonge7083@naver.com>
2023-02-16 18:00:30 +0900

    0216

---
+++ js/test.js
@@ -0,0 +1,18 @@
+var user_name = "Maktub";
+var user_num = 1123; 
+document.write('javascript이름=', user_name, '<br>');
+document.write('javascript숫자=', user_num);
+document.write('<hr size=5 color=blue> ')
+
+//국어=90 영어=85 총점=175 평균=87.5
+//문제1 변수에 값대입, 연산 - 더하기,몫
+
+var kor= 90;
+var Eng= 85;
+var total= kor + Eng;
+var avg = total/2 ;
+
+document.write('국어점수 =', kor, '<br>');
+document.write('영어점수=', Eng, '<br>' );
+document.write('총점점수=', total, '<br>');
+document.write('평균점수=', avg, '<br>');
